d1701471536e0952a40547f7450f52c6741ef6ea
jennifer-fintlabs <jennifer@fintlabs.no>
2023-05-12 14:29:15 +0200

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('fint-admin-portal-appbar-pod') as HTMLElement
 );
 root.render(
   <React.StrictMode>
